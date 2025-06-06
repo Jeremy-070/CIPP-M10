119894d411a4989b7fb233fbd3737349be261a3f
Jeremy-070 <jeremy.harris@mach-10.com>
2025-06-06 15:43:44 +0200

    Update logo.js
    Signed-off-by: Jeremy-070 <jeremy.harris@mach-10.com>

--- src/components/logo.js
+++ src/components/logo.js
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 export const Logo = (props) => {
   // Replace the base64 string with a path to your new logo in the public folder
-  const i = "/Mach 10_Logo_White.png"; 
+  const i = "/mach10.svg"; 
   
   // The original check for "LsS2ECuxvo" is removed as it's related to the old base64 string.
   // If you want to add a check for the new logo, you can implement it here.
